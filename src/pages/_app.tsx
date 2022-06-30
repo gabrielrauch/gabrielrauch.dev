99633882889caefca8810ad257fadb6587ba12e1
gabrielrauch <gabrielrauch@protonmail.com>
2022-06-29 22:35:56 -0300

    Remove unused Head on _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from 'next/app'
 import GlobalCss from 'global/Styles/GlobalStyles'
-import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
